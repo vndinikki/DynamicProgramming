a561d4a736509b84a22d5850442eff42ca245068
Nikita Bhargava <31789913+vndinikki@users.noreply.github.com>
2021-12-12 09:11:00 +0530

    Create DivisorGame.js

---
+++ DivisorGame.js
@@ -0,0 +1,11 @@
+// 1025. Divisor Game
+// https://leetcode.com/problems/divisor-game/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var divisorGame = function(n) {
+    return n%2 == 0 ? true: false;
+};
